c1087499282b9f1e6118ffc866d4bbe71dad349f
Ian Andrian Tanuwidjaja <ian.tanuwidjaja@gmail.com>
2022-04-18 20:11:31 -0700

    sixth commit

--- ece140_cloud_deployment-master/web/src/server.py
+++ ece140_cloud_deployment-master/web/src/server.py
@@ -21,7 +21,7 @@ def get_home(req):
   return render_to_response('templates/home.html', {'users': records}, request=req)
 
 def get_page1(req):
-  return FileResponse('templates/page1.html', request=req)
+  return FileResponse('templates/page1.html')
 ''' Route Configurations '''
 if __name__ == '__main__':
   config = Configurator()
